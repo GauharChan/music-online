88c584f20e5b2f53a2b0b5b1d3300b61178cd40f
GauharChan <gauharchan@163.com>
2019-12-29 09:40:45 +0800

    完成布局，下一步写搜索组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  height: 100vh;
+  background: url("./assets/bg.jpg") no-repeat;
+  background-size: 100% 100%;
+  padding-top: 0.1px;
 }
 
-@media screen and (min-width: 768px){
-  html{
-    font-size: 16px
+.flex {
+  display: flex;
+}
+
+.gh-between {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.gh-around {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.gh-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@media screen and (min-width: 768px) {
+  html {
+    font-size: 20px;
   }
 }
 
-@media screen and (max-width: 414px){
-  html{
-    font-size: 10px
+@media screen and (max-width: 414px) {
+  html {
+    font-size: 10px;
   }
 }
-
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,61 @@
 <template>
   <div class="home">
-    
+    <header class="gh-between">
+      <div class="title">悦听</div>
+      <Search />
+    </header>
+    <main class="flex">
+      <div class="music-list"></div>
+      <div class="center-wall"></div>
+      <div class="comment"></div>
+    </main>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Search from '../components/Search'
 export default {
   name: 'home',
+  components:{
+    Search
+  }
 }
 </script>
+<style lang="less" scoped>
+  .home{
+    width: 70%;
+    height: 70vh;
+    margin: 15vh auto 0;
+    background-color: rgba(255, 255, 255, .2);
+    // header
+    header{
+      padding: 1rem;
+      background-color: #13a1df;
+
+      .title{
+        color: #fff;
+        font-size: 1rem;
+      }
+    }
+    // mian
+    main{
+      height: 100%;
+      .music-list{
+        width: 25%;
+        height: 100%;
+        background-color: red;
+      }
+      .center-wall{
+        width: 50%;
+        height: 100%;
+        background-color: green;
+      }
+      .comment{
+        width: 25%;
+        height: 100%;
+        background-color: blue;
+      }
+    }
+  }
+</style>
